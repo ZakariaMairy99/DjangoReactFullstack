0c2774bfef9da315a3f901b43804bb354b14e4df
Zakaria Mairy <zikossezm@gmail.com>
2024-12-17 20:45:28 +0100

    My first commit

---
+++ reactFrontend/src/components/Form.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import api from "../api";
+import { useNavigate } from "react-router-dom";
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
+import "../styles/Form.css";
+import imageLeft from "../assets/gg.png"; // Adjust the path to your image
+
+function Form({ route, method }) {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+
+    const name = method === "login" ? "Login" : "Register";
+
+    const handleSubmit = async (e) => {
+        setLoading(true);
+        e.preventDefault();
+
+        try {
+            console.log("Submitting to:", route);
+            const res = await api.post(route, { username, password });
+            if (method === "login") {
+                localStorage.setItem(ACCESS_TOKEN, res.data.access);
+                localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
+                navigate("/");
+            } else {
+                navigate("/login");
+            }
+        } catch (error) {
+            console.error("Error occurred:", error);
+            alert(error.message || "An error occurred");
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return (
+        <div className="form-page-container">
+            <div className="image-container">
+                <img src={imageLeft} alt="Login Illustration" className="left-image" />
+            </div>
+            <form onSubmit={handleSubmit} className="form-container">
+                <h1>{name}</h1>
+                <input
+                    className="form-input"
+                    type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    placeholder="Username"
+                />
+                <input
+                    className="form-input"
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="Password"
+                />
+                <button className="form-button" type="submit" disabled={loading}>
+                    {loading ? "Loading..." : name}
+                </button>
+            </form>
+        </div>
+    );
+}
+
+export default Form;
